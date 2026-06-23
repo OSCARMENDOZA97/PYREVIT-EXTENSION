8431b3a1a5aa2ef16903d09402fff77d9d8f2a21
OSCARMENDOZA97 <mw529102@gmail.com>
2026-06-23 12:05:16 -0500

    Update script.py

--- OM-BIM.extension/OMTools.tab/Dev.panel/RoomToViews.pushbutton/script.py
+++ OM-BIM.extension/OMTools.tab/Dev.panel/RoomToViews.pushbutton/script.py
@@ -36,7 +36,7 @@ Author: Oscar Mendoza"""
 ### EXTRA: Tú puedes borrar esto
 __helpUrl__ = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 __min_revit_ver__ = 2021
-__max_revit_ver__ = 2026
+__max_revit_ver__ = 2027
 
 
 
